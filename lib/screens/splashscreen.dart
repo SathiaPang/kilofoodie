85525f369013831eb2e5ae6745f4aeafe91fba37
Sathia Pang <pangvichetsatya1@gmail.com>
2024-01-01 15:34:21 +0700

    hello

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kilofoodie/constants.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,7 +7,19 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Constants.primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 250,
+              height: 250,
+              child: Image.asset('assets/images/logo-foodie.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
